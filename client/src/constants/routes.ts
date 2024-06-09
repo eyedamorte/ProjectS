1296fae31307f3a279af8accab50a1a62035580d
Eyedamorte <30478301+eyedamorte@users.noreply.github.com>
2024-06-09 23:09:13 +0500

    add Next and FSD

--- client/src/constants/routes.ts
+++
@@ -1,3 +0,0 @@
-export const SIGNIN_ROUTE = '/signin';
-export const REGISTRATION_ROUTE = '/registration';
-export const HOME_ROUTE = '/home';
--- client/src/pages/SigninPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const SigninPage = () => {
-  return <div>AUTH</div>;
-};
